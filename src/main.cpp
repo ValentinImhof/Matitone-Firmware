f9cd8053f5a36e5b4e79f71a9ed1d6be662a19df
Valentin Imhof <valentin.imhof@heig-vd.ch>
2025-04-28 13:39:54 +0200

    ajout du main de demonstration

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "matitone.h"
-// Test Noé Commit
+/* Main actuel
 void setup()
 {
   Serial.begin(9600);  // Initialise le port série pour le débogage
@@ -46,4 +46,55 @@ void loop()
   }
   
   delay(100);  // Délai d'une seconde entre les lectures
+}*/
+
+//----------------------------------------------------------------------------------------------------------
+
+/*Main pour la démo mardi soir
+les capts de pression allument leur led IR respective
+envoi un message si bouton pressé, bouton 1 en rising edge, bouton 2 en continu*/
+
+bool S1prev = false; // État précédent du bouton 1
+void setup()
+{
+  Serial.begin(9600);  // Initialise le port série pour le débogage
+  BtSetup();  // Appelle la fonction de configuration Bluetooth
+  SetupCapt();  // Appelle la fonction de configuration du capteur
+  pinMode(2, OUTPUT); // Configure la broche 2 comme sortie pour la LED IR AV
+  pinMode(3, OUTPUT); // Configure la broche 3 comme sortie pour la LED IR AR
+  pinMode(4, INPUT); // Configure la broche 4 comme entrée pour le bouton 1
+  pinMode(5, INPUT); // Configure la broche 5 comme entrée pour le bouton 2
+}
+
+void loop()
+{
+  BtLoop();  // Appelle la fonction de boucle Bluetooth
+  if(ReadCapt("AV") > 1) {  // Si la valeur du capteur avant est supérieure à 1
+    Serial.println("Capteur avant activé");
+    digitalWrite(2, HIGH); // Allume la LED IR AV
+  }
+  else {
+    digitalWrite(2, LOW); // Éteint la LED IR AV
+  }
+
+  if(ReadCapt("AR") > 1) {  // Si la valeur du capteur arrière est supérieure à 1
+    Serial.println("Capteur arrière activé");
+    digitalWrite(3, HIGH); // Allume la LED IR AR
+  }
+  else {
+    digitalWrite(3, LOW); // Éteint la LED IR AR
+  }
+
+  if((digitalRead(4) == HIGH) && (S1prev == false)) {  // Si le bouton sur la broche 4 est pressé
+    Serial.println("Bouton 1 pressé"); //envoie une fois
+    BtSend("S1");
+  }
+  S1prev = digitalRead(4); // Met à jour l'état précédent du bouton 1
+
+  if (digitalRead(5) == HIGH) {  // Si le bouton sur la broche 5 est pressé
+    Serial.println("Bouton 2 pressé");
+    BtSend("S2"); //envoie en continu
+  }
+
+  delay(10);  // Délai entre les lectures
 }

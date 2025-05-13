b410e5347ff804db1892bde3a465c83e5f6842ec
Valentin Imhof <valentin.imhof@heig-vd.ch>
2025-05-13 18:02:50 +0200

    essai lowpower

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,37 @@
 #include "matitone.h"
-//Main actuel
-void setup()
-{
-  Serial.begin(9600);  // Initialise le port série pour le débogage
-  BtSetup();  // Appelle la fonction de configuration Bluetooth
-  SetupCapt();  // Appelle la fonction de configuration du capteur
-  SetupAccel();  // Appelle la fonction de configuration de l'accéléromètre
-  pinMode(4, INPUT); // Configure la broche 4 comme entrée pour le bouton 1
-  pinMode(5, INPUT); // Configure la broche 5 comme entrée pour le bouton 2
-  pinMode(6, INPUT); // Configure la broche 6 comme entrée pour le bouton 3
+#include "ArduinoLowPower.h"
+
+unsigned long previousMillis = 0;
+const unsigned long interval = 100;  // lecture toutes les 100 ms
+
+void setup() {
+  Serial.begin(9600);
+  SetupAccel();  // initialisation de l'IMU
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Désactive le port série quand inactif pour réduire la conso
+  LowPower.attachInterruptWakeup(RTC_ALARM_WAKEUP, []{}, CHANGE);
 }
 
-void loop()
-{
-  BtLoop();  // Appelle la fonction de boucle Bluetooth
+void loop() {
+  unsigned long currentMillis = millis();
 
-  float accelX, accelY, accelZ;
-  if (ReadAccel(accelX, accelY, accelZ)) {
-        if((accelX > 1.2) or (accelY > 1.2) or (accelZ > 1.2)) {  // Si l'accélération dépasse 1.2g
-            Serial.println("Mouvement détecté");
-            BtSend("Mouvement");
-        }
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    float accelX, accelY, accelZ;
+    if (ReadAccel(accelX, accelY, accelZ)) {
+      if ((accelX > 1.2) || (accelY > 1.2) || (accelZ > 1.2)) {
+        Serial.println("Mouvement détecté");
+        digitalWrite(LED_BUILTIN, HIGH);
+      } else {
+        digitalWrite(LED_BUILTIN, LOW);
+      }
+    } else {
+      digitalWrite(LED_BUILTIN, LOW);
     }
-
-  if (ReadCapt("AV") > 2) {  // Si la valeur du capteur avant est supérieure à 1
-    Serial.println("Capteur avant activé");
-    BtSend("PressionAV"); 
   }
 
-  if (ReadCapt("AR") < 1.5) {  // Si la valeur du capteur arrière est inférieure à 1 (avec resistance)
-    Serial.println("Capteur arrière activé");
-    BtSend("PressionAR");
-  }
-
-  if (digitalRead(4) == HIGH) {  // Si le bouton sur la broche 4 est pressé
-    Serial.println("Bouton 1 pressé");
-    BtSend("Bouton1");
-  }
-  if (digitalRead(5) == HIGH) {  // Si le bouton sur la broche 5 est pressé
-    Serial.println("Bouton 2 pressé");
-    BtSend("Bouton2");
-  }
-  if (digitalRead(6) == HIGH) {  // Si le bouton sur la broche 6 est pressé
-    Serial.println("Bouton 3 pressé");
-    BtSend("Bouton3");
-  }
-  
-  delay(100);  // Délai d'une seconde entre les lectures
+  // Mise en veille "léger" entre chaque boucle
+  LowPower.idle();
 }
